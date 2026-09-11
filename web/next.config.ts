f763c82f54570577efc0fcc810830c2188e4e44a
datgiaodau <phamtuandat50@gmail.com>
2026-09-12 00:42:44 +0700

    fix deploy

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,3 +1,6 @@
 import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl=createNextIntlPlugin('./src/i18n/request.ts');
-export default withNextIntl({output:'standalone',agentRules:false});
+export default withNextIntl({
+  output: process.env.VERCEL ? undefined : 'standalone',
+  agentRules: false
+});
